629860f7d71353ab2e1c063b6eb877af0169f932
Egorrr3 <egorrr170@gmail.com>
2023-11-13 21:04:46 +0200

    Initial commit

---
+++ AirlinesAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AirlinesAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
